87b0d32462d5b4b364beffc8cbb32cb83229ea69
yeison herbert <yeisonherbert@gmail.com>
2021-12-15 16:07:14 -0500

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FomrularioComponent } from './fomrulario/fomrulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FomrularioComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
